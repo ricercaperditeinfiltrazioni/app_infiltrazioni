42904b303856803797bf396d6376371da0aa8cc9
ricercaperditeinfiltrazioni <infiltrazioni2011@gmail.com>
2026-03-30 08:56:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/relazione_provider.dart';
-import 'screens/archivio_screen.dart'; // ORA PARTE DALL'ARCHIVIO!
+import 'screens/archivio_screen.dart';
+import 'screens/dati_cantiere_screen.dart';
+import 'screens/problematiche_screen.dart';
+import 'screens/gas_traccianti_screen.dart';
+import 'screens/verifiche_strumentali_screen.dart';
+import 'screens/ripristino_iniezioni_screen.dart';
+import 'screens/vulnerabilita_screen.dart';
+import 'screens/cause_consigli_screen.dart';
 
 void main() {
   runApp(
@@ -24,7 +31,16 @@ class AppInfiltrazioni extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ArchivioScreen(), // HOME CAMBIATA
+      home: const ArchivioScreen(),
+      routes: {
+        '/dati_cantiere':       (ctx) => const DatiCantiereScreen(),
+        '/problematiche':       (ctx) => const ProblematicheScreen(),
+        '/gas_screen':          (ctx) => const GasTraccianiScreen(),
+        '/igrometria_screen':   (ctx) => const VerificheStrumentaliScreen(),
+        '/termografia_screen':  (ctx) => const RipristoIniezioniScreen(),
+        '/vulnerabilita_screen':(ctx) => const VulnerabilitaScreen(),
+        '/cause_consigli':      (ctx) => const CauseConsigliScreen(),
+      },
     );
   }
 }
